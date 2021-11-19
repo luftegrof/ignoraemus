5f758113f8b1bb871520dac893d1ce4a5f13ff10
luftegrof <69501887+luftegrof@users.noreply.github.com>
2021-11-19 03:27:56 +0000

    handle additional signals

--- ignoraemus.go
+++ ignoraemus.go
@@ -9,11 +9,15 @@ import (
 
 func main() {
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
 	for signal := range c {
 		switch signal {
 			case syscall.SIGHUP:
 				fmt.Printf("\nNo, you %s first!\n", signal)
+			case syscall.SIGINT:
+				fmt.Printf("\nDo NOT %s me!\n", signal)
+			case syscall.SIGTERM:
+				fmt.Printf("\nI'll be back.\n")
 		}
 	}
 }
