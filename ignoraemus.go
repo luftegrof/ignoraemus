1af66e24b6ea46de27be7c71c9341d6d38ef8140
luftegrof <69501887+luftegrof@users.noreply.github.com>
2021-11-19 03:39:11 +0000

    do something while waiting for signals

--- ignoraemus.go
+++ ignoraemus.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,14 +11,20 @@ import (
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
-	for signal := range c {
-		switch signal {
-			case syscall.SIGHUP:
-				fmt.Printf("\nNo, you %s first!\n", signal)
-			case syscall.SIGINT:
-				fmt.Printf("\nDo NOT %s me!\n", signal)
-			case syscall.SIGTERM:
-				fmt.Printf("\nI'll be back.\n")
+	go func() {
+		for signal := range c {
+			switch signal {
+				case syscall.SIGHUP:
+					fmt.Printf("\nNo, you %s first!\n", signal)
+				case syscall.SIGINT:
+					fmt.Printf("\nDo NOT %s me!\n", signal)
+				case syscall.SIGTERM:
+					fmt.Printf("\nI'll be back.\n")
+			}
 		}
+	}()
+	for true {
+		fmt.Println("Just doing my thing...")
+		time.Sleep(5 * time.Second)
 	}
 }
